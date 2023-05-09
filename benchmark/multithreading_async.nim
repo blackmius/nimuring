3a64d389dcdac9a2adb8162fdd37b28cd8e467bb
dterlyakhin <blackmius@gmail.com>
2023-05-09 15:57:49 +0300

    Добавил свои результаты прогона

--- benchmark/multithreading_async.nim
+++ benchmark/multithreading_async.nim
@@ -1,5 +1,13 @@
+# Intel(R) Core(TM) i7-10850H CPU @ 2.70GHz
+# threads: 1 count: 7 733 497 count per thread: 7 733 497
+# threads: 2 count: 12 004 674 count per thread: 6 002 337
+# threads: 4 count: 16 913 595 count per thread: 4 228 398.75
+# threads: 6 count: 18 863 626 count per thread: 3 143 937
+# threads: 8 count: 17 061 970 count per thread: 2 132 746.25
+# threads: 10 count: 15 984 601 count per thread: 1 598 460.1
+# threads: 12 count: 15 899 753 count per thread: 1 324 979.416
+
 import atomics
-import cpuinfo
 import nimuring/async
 
 var i: Atomic[int]
@@ -17,7 +25,7 @@ proc worker() {.thread.} =
     asyncCheck run()
   runForever()
 
-let cpus = countProcessors()
+let cpus = 12
 var pool = newSeq[Thread[void]](cpus)
 for i in 0..pool.high:
   pool[i].createThread(worker)
